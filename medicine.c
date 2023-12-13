334100f445eb8e05427db94b99cfc0d57e89a8a9
Heikey <joaofialho123123@gmail.com>
2023-12-13 14:30:14 -0300

    Medicine: Update Medicine struct and functions
    Hopefully i don't have to update more of this

--- medicine.c
+++ medicine.c
@@ -1,14 +1,14 @@
 #define _XOPEN_SOURCE
-#define _GNU_SOURCE
-#include <string.h>
 #include "medicine.h"
 #include "database.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define BUFF_MAX 128
 
+<<<<<<< HEAD
 // Essa função remove o \n da string
 inline void trim(char* string) {
 	if(string[strlen(string)-1] == '\n')
@@ -85,4 +85,108 @@ void medicine_deserialize(char* string, Medicine* med) {
 	char* code = strsep(&value,"\", ");
 	char* price = strsep(&value,"\", ");
 	char* date = strsep(&value,"\", ");
+=======
+void time_read(Time *time_, int now) {
+  struct tm day_tm;
+  if (now == 1) {
+    time_t now_ = time(NULL);
+    day_tm = *localtime(&now_);
+  } else {
+    printf("Digite o dia da compra:");
+    char day_str[32];
+    fflush(NULL);
+    fgets(day_str, 32, stdin);
+    strptime(day_str, "%d/%m/%Y", &day_tm);
+  }
+  time_->day = day_tm.tm_mday;
+  time_->month = day_tm.tm_mon + 1;
+  time_->year = day_tm.tm_year + 1900;
+}
+
+int time_cmp(Time *t1, Time *t2) {
+  return t1->day == t2->day && t1->month == t2->month && t1->year == t2->year;
+}
+
+void time_print(Time *time) {
+  printf("%d/%d/%d", time->day, time->month, time->year);
+}
+
+Medicine *medicine_alloc() {
+  Medicine *out = (Medicine *)malloc(sizeof(Medicine));
+  return out;
+}
+
+inline void trim(char *string) {
+  if (string[strlen(string) - 1] == '\n')
+    string[strlen(string) - 1] = '\0';
+}
+
+int medicine_read(Medicine *med) {
+  printf("Escreva o nome do produto:");
+  fflush(stdin);
+  fgets(med->name, BUFF_MAX, stdin);
+  trim(med->name);
+
+  printf("Escreva o nome do laboratorio:");
+  fflush(stdin);
+  fgets(med->lab, BUFF_MAX, stdin);
+  trim(med->lab);
+
+  printf("Escreva o nome do fornecedor:");
+  fflush(stdin);
+  fgets(med->supplier, BUFF_MAX, stdin);
+  trim(med->supplier);
+
+  printf("Digite o codigo do produto:");
+  scanf("%li", &med->code);
+
+  printf("Digite o preço do produto:");
+  scanf("%f", &med->price);
+
+  printf("Foi comprado hoje?[S/N]");
+  Time buy_date;
+  while (1) {
+    char awns;
+    scanf("%c", &awns);
+    if (awns == 's') {
+      time_read(&buy_date, 1);
+      break;
+    } else if (awns == 'n') {
+      time_read(&buy_date, 0);
+      break;
+    }
+  }
+  med->buy_date = buy_date;
+  return 1;
+}
+
+void medicine_print(Medicine *med) {
+  printf("Nome: %s\n", med->name);
+  printf("Laboratorio: %s\n", med->lab);
+  printf("Fornecedor: %s\n", med->supplier);
+  printf("Codigo: %lil\n", med->code);
+  printf("Preço: %.2f\n", med->price);
+  time_print(&med->buy_date);
+}
+
+int medicine_identity(void *med) { return ((Medicine *)med)->ID; }
+
+// int func(val,arg)
+
+int filter_by_id(void *val, void *arg) {
+  return ((Medicine *)val)->ID == *(int *)arg;
+}
+
+int filter_by_name(void *val, void *arg) {
+  return strcmp(((Medicine *)val)->name, (char *)arg) == 0;
+}
+int filter_by_lab(void *val, void *arg) {
+  return strcmp(((Medicine *)val)->lab, (char *)arg) == 0;
+}
+int filter_by_supplier(void *val, void *arg) {
+  return strcmp(((Medicine *)val)->supplier, (char *)arg) == 0;
+}
+int filter_by_buy_date(void *val, void *arg) {
+  return time_cmp(&((Medicine *)val)->buy_date, (Time *)arg);
+>>>>>>> Medicine: Update Medicine struct and functions
 }
--- medicine.h
+++ medicine.h
@@ -1,10 +1,34 @@
 #ifndef __MEDICINE_H
 #define __MEDICINE_H
-	
-typedef struct TagMedicine Medicine;
-Medicine* medicine_alloc();
-int medicine_read(Medicine* med);
-char *medicine_serialize(Medicine *med); 
 
+typedef struct TagTime {
+  int day;
+  int month;
+  int year;
+} Time;
+
+typedef struct TagMedicine {
+  int ID;
+  char name[64];
+  char lab[64];
+  char supplier[64];
+  long int code;
+  float price;
+  Time buy_date;
+} Medicine;
+
+void time_read(Time *time_, int now);
+int time_cmp(Time *t1, Time *t2);
+
+Medicine *medicine_alloc();
+int medicine_read(Medicine *med);
+
+int medicine_identity(void *med);
+
+int filter_by_id(void *val, void *arg);
+int filter_by_name(void *val, void *arg);
+int filter_by_lab(void *val, void *arg);
+int filter_by_supplier(void *val, void *arg);
+int filter_by_buy_date(void *val, void *arg);
 
 #endif
